b847096cbf66402ab1b3ea04eb9e63363d975dd8
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-05 18:57:16 -0700

    K.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "This is an error message to stderr.\n");
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
