e49fa2dcf7a8a8840bfaca81c5bd1b436b4b4251
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-05 18:52:15 -0700

    K.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main() {
+    fprintf(stderr, "This is an error message to stderr.\n");
+    return (1);
+}
+
