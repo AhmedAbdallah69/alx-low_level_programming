78b032a664c68078e9ab37f75c7889ff0a1155df
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-08 16:18:14 -0700

    K.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Uses a for loop to determine if a number is positive or not
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch1 = 'a', ch2 = 'A';
+	int i = 0;
+	
+	while (i < 26)
+	{
+		putchar(ch1);
+		ch1++;
+		i++;
+	}
+	i = 0;
+	 while (i < 26)
+        {
+                putchar(ch2);
+                ch2++;
+                i++;
+        }
+	putchar('\n');
+	return (0);
+}
