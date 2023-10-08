47f4eeb9ef2507699cb878d8b02293857ebc9fb7
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-08 16:23:58 -0700

    K.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,19 +8,23 @@ int main(void)
 {
 	char ch1 = 'a', ch2 = 'A';
 	int i = 0;
+
 	while (i < 26)
 	{
-	putchar(ch1);
-	ch1++;
-	i++;
+		putchar(ch1);
+		ch1++;
+		i++;
 	}
+
 	i = 0;
-	 while (i < 26)
+
+	while (i < 26)
         {
-	putchar(ch2);
-	ch2++;
-	i++;
+		putchar(ch2);
+		ch2++;
+		i++;
         }
+
 	putchar('\n');
 	return (0);
 }
