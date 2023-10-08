9f6d599707d0203d0551d1b23f49574b0f94720f
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-08 16:57:27 -0700

    K.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Uses a for loop to determine if a number is positive or not
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
