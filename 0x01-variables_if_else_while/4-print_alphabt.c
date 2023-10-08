01c099b2c7756a3ebe8b2e0dfe3af2f92f92d678
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-08 16:36:57 -0700

    K.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Uses a for loop to determine if a number is positive or not
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 26)
+	{
+		if(i != 4 && i != 16){
+		putchar(ch);
+	}
+		ch++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
