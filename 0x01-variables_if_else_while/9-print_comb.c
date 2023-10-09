849f180ee166c69fea28864ba75da1c92ab0c1c4
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-08 17:31:46 -0700

    K.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@ int main(void)
 {
 	int i = 48;
 
-	while (i < 57)
+	while(i < 58)
 	{
 		putchar(i);
+		if(i == 57)
+			break;
 		putchar(',');
 		putchar(' ');
 		i++;
 	}
-	putchar(i);
 	return (0);
 }
