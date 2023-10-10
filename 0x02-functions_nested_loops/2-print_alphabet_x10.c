c7e96d317d57b5ec44267ea57befb376d50cb7d2
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-10 13:20:26 -0700

    K.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_alphabet - a function to print the alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 26)
+	{
+	_putchar(ch);
+	ch++;
+	i++;
+	}
+	_putchar('\n');
+}
