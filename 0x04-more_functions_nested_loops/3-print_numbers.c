8ec80582a1b7c20eef6db53e394a8264516178f8
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-12 14:58:34 -0700

    K.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_numbers - a function that multiplies two integers.
+ * Return: Always int.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar ('0' + i);
+
+	_putchar ('\n');
+}
