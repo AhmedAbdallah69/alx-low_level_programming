3c5216d1c554d34e4a20cc5110a3c9692b344939
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-16 15:25:31 -0700

    K.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - a function that prints a string similar to 'puts'.
+ * @str: string to be acted upon.
+ * Return: Always void.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+		putchar(s[i]);
+}
