5ea96bb71ab4b8a4827efd0766e1f764340e2df6
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-16 20:05:13 -0700

    K.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random character sequence to meet specific sum criteria.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int sum = 0;
+    char c;
+
+    srand(time(NULL));
+
+    while (sum <=2648)
+    {
+        c = rand() % 128;
+        sum += c;
+        putchar(c);
+    }
+
+    putchar(3269 - sum);
+    return (0);
+}
