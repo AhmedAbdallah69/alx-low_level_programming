8fe7c1e817300231c9219fab3447f02b16cbd7f8
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-16 18:06:36 -0700

    K.

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@ char *_strcpy(char *dest, char *src)
 int size, i, j;
     for (i = 0;src[i]!='\0'; ++i);
     size = i + 1;
-    for (j = 0; j < size; ++j) {
+    for (j = 0; j < i; ++j) {
         dest[j] = src[j];
     }
     return(dest);
