8a0e18dd5b29a3253d9f35ea45d54c12e4b6c5e8
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-19 18:33:20 -0700

    K.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "bootcamp.h"
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference of first characters that are of different value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+}
