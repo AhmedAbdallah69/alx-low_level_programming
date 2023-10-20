0d79bde70bf56bcae0edb31be9347cbf02530b15
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-19 17:56:31 -0700

    K.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *_strncpy - Write a function that concatenates two strings.
+ *
+ *@dest: This is the output dest
+ *@src: This is the input source
+ *@n: int
+ * Return: This return to dest, that concatenates two strings
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; ++i)
+	{
+		dest[i] = src[i];
+	}
+	if (i < n)
+		dest[i] = '\0';
+	return (dest);
+}
