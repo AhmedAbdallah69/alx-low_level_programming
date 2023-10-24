4a95681c73ec25641390875d48e81a3870470c16
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-23 18:25:13 -0700

    K.

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_diagsums - print the sum the diognals of a square matrix of integer
+ *
+ * @a: This is the array to sum
+ * @size: This is the length of the array
+ */
+void print_diagsums(int *a, int size)
+{
+	int primary_diag_sum = 0;
+	int secondary_diag_sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		primary_diag_sum += a[i * size + i];
+		secondary_diag_sum += a[i * size + size - i - 1];
+	}
+
+	printf("%d, d\n", primary_diag_sum, secondary_diag_sum);
+}
