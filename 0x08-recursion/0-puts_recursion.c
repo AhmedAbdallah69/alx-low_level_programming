288227dab2a126ce60762f276f6009f96346de69
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-10-25 15:20:18 -0700

    K.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a whole string using recursion.
+ * @s: strin to be operated on.
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if ((*s) == '\0')
+		return;
+	putchar(*s);
+	_puts_recursion(++s);
+}
