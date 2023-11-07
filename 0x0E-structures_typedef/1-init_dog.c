be957aeddc593da4cf85f601510c893a946ea715
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-06 17:12:32 -0800

    K.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->owner = owner;
-	d->name = name;
-	d->age = age;
+	if (d)
+	{
+		d->owner = owner;
+		d->name = name;
+		d->age = age;
+	}
 }
