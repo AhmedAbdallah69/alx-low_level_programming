a67ce999e72a5ed467bd7a7296ed05f3df9598bc
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-06 17:48:08 -0800

    K.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(new_dog));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
