1770c45644fe33f57d28c2df7d8f719dbee9eadd
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-17 01:44:23 +0200

    K.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - Outputs all elements of a list_t list.
+ *
+ * @h: Pointer to the head of the list
+ *
+ * Return: Number of nodes in the list
+ *         If str is NULL, prints [0] (nil)
+ */
+
+size_t print_list(const list_t *h)
+{
+    unsigned int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%u] (nil)\n", h->len);
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		count += 1;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * len - Calculates the length of a string.
+ *
+ * @str: Constant string
+ *
+ * Return: The length of the string
+ */
+int len(const char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ *
+ * @head: Pointer to the head of the linked list
+ * @str: String to be assigned to the node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+    }
+
+	new_node->len = len(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
