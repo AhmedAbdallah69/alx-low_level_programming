d38d76e19c2e34528ce2d68f2da6cbff4c60c596
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-17 02:02:15 +0200

    K.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,7 +28,7 @@ int len(const char *str)
  *
  * Return: The address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, char *str)
 {
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 
