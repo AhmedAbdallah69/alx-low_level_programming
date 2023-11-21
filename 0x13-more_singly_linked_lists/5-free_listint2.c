b41f5d9f9a563d237349fd6030a8524b9fcae4f6
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-21 11:20:25 -0800

    K.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Free nodes and list
+ * @head: This is my struct
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+	next = current->next;
+		free(current);
+		current = next;
+	}
+	head = NULL;
+}
