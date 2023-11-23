f102221a224f643bf1bbb70fbb0c77a5f1f747d1
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-23 13:22:08 -0800

    K.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - This funtion convert binary to decimal
+ * @b: This is my string of entry
+ * Return: This return to a digital number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int n = 0;
+    int j = 1, i;
+
+    if (!b)
+        return (0);
+
+    for (i = 0; b[i] != '\0'; ++i)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+    }
+
+    for (i = i - 1; i >= 0 ; i--)
+    {
+        n += (b[i] - '0') * j;
+        j *= 2;
+    }
+
+    return (n);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef BIT_MANIPULATION_MAIN_H
+#define BIT_MANIPULATION_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
