3a464e8ee9ce519611fb6ffeac28abce37045376
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-23 13:24:07 -0800

    K.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int n = 0;
-    int j = 1, i;
+	unsigned int n = 0;
+	int j = 1, i;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (i = 0; b[i] != '\0'; ++i)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-    }
+	for (i = 0; b[i] != '\0'; ++i)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+	}
 
-    for (i = i - 1; i >= 0 ; i--)
-    {
-        n += (b[i] - '0') * j;
-        j *= 2;
-    }
+	for (i = i - 1; i >= 0 ; i--)
+	{
+		n += (b[i] - '0') * j;
+		j *= 2;
+	}
 
-    return (n);
+	return (n);
 }
