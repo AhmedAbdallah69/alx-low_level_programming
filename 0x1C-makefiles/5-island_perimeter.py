b5ea074863e8338d7ad92f7f268f17da22f5ea0a
AhmedAbdallah69 <aljwkrl@gmail.com>
2024-03-24 23:11:09 +0200

    K.

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""
-Thechincal interview preparation
-This module that returns the perimeter of the island describ in grid
-"""
+'''task 5 module'''
 
 
 def island_perimeter(grid):
-    """ THis functions returns the
-        perimeter of the island described in grid """
-    perimeter = 0
-    for i, row in enumerate(grid):
-        for j, col in enumerate(row):
-            if grid[i][j] == 1:
-                perimeter = perimeter + 4
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter = perimeter - 2
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter = perimeter - 2
-    return perimeter
+    '''returns the perimeter of the island described in grid'''
+    total = 0
+    last_fc = []  # last list found cells indexes
+    current_fc = []
+    for index, list in enumerate(grid):
+        for idx, cell in enumerate(list):
+            if cell == 1:
+                current_fc.append(idx)
+                # right and left sides
+                if idx == 0 or list[idx - 1] == 0:
+                    total += 1  # add left side of the cell
+                if idx == len(list) - 1 or list[idx + 1] == 0:
+                    total += 1  # add  right side of the cell
+                # top sides
+                if idx not in last_fc:
+                    total += 1  # add the top side
+                # bottom sides
+                if index + 1 < len(grid):
+                    if grid[index + 1][idx] != 1:
+                        total += 1  # add bottom side of the cell
+                else:
+                    total += 1
+        last_fc = current_fc.copy()
+        current_fc = []
+
+    return total
